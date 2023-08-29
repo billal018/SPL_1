99abe8baa31acaa484d427f4fa7270b150bd05db
Billal018 <billalmia297222@gmail.com>
2023-08-29 11:39:07 +0600

    resume Added

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        //System.out.println("\uD83D\uDE8A");
+        string armyOfficerEmoji = "\uD83C\uDFD4\uFE0F";
+        System.out.println(armyOfficerEmoji);
+
+       // System.out.println(skullEmoji);
+    }
+}
+
+
+
+
+
+
+/////deyal="\uD83E\uDDF1"
+/*
+man=="\uD83C\uDF85"
+soldier==🪖="\uD83E\uDE96"
+ninja=🥷=="\uD83E\uDD77"
+enemy=🧌="\uD83E\uDDCC"
+
+ */
